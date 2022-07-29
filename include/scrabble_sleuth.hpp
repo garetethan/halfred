f5b70c6b7e990ba9786f3ca500b59fad60630127
garetethan <15016851+garetethan@users.noreply.github.com>
2022-07-29 12:00:14 -0700

    v1 basic: Implement turn(), best_overall(), etc.
    Continue implementing the basic implementation from v1 of the
    proposal. Implement turn(), parse_location(), best_overall(),
    apply_move_to_board().

--- include/scrabble_sleuth.hpp
+++ include/scrabble_sleuth.hpp
@@ -5,11 +5,16 @@
 #include <fstream>
 #include <iterator>
 #include <regex>
+#include <set>
+#include <stdexcept>
 #include <string>
 #include <vector>
 
 namespace scrabble_sleuth {
 	using size_type = unsigned int;
+
+	const std::string person_word_prompt{"What word do you want to play?"};
+	const std::string person_location_prompt{"Where do you want to play the word?"};
 
 	std::ifstream defensively_open(std::string path) {
 		std::ifstream file_{path};
@@ -19,18 +24,27 @@ namespace scrabble_sleuth {
 		return file_;
 	}
 
+	std::string get_input(std::string prompt, std::istream& in = std::cin, std::ostream& out = std::cout) {
+		out << prompt << " " << std::flush;
+		std::string input;
+		in >> input;
+		return input;
+	}
+
 	class ScrabbleGame {
 		public:
 
-		constexpr static unsigned int letter_offset = 97;
-		constexpr static size_type letter_space_size = 26;
-		constexpr static size_type available_letter_sum = 8;
-		constexpr static char empty = '_';
-		constexpr static char wild = '*';
+		static constexpr unsigned int letter_offset = 97;
+		static constexpr size_type letter_space_size = 26;
+		static constexpr size_type available_letter_sum = 8;
+		static constexpr char empty = '_';
+		static constexpr char wild = '*';
+		static const std::regex valid_location;
 
 		struct play {
 			size_type row;
 			size_type col;
+			bool across;
 			std::string word;
 			int score;
 		};
@@ -44,39 +58,50 @@ namespace scrabble_sleuth {
 			}
 		}
 
-		play best_in_row(std::vector<char>& board_row) {
-			std::vector<char> row_letters{};
-			std::copy_if(board_row.begin(), board_row.end(), std::back_inserter(row_letters), [](auto c){return c != empty;});
-			play best_play{0, 0, "no matches found", -1};
-			for (std::string word : valid_words_) {
-				if (word.size() > board_dimension_) {
-					continue;
+		bool turn(std::string person_word, std::string person_location, std::ostream& out = std::cout) {
+			play person_play = parse_location(person_location);
+			while (person_play.score <= 0) {
+				while (person_word.empty()) {
+					out << "Invalid word. Be sure to use only lowercase English letters." << std::endl;
+					person_word = clean_word(get_input(person_word_prompt));
 				}
-				for (auto letter = row_letters.begin(); letter != row_letters.end(); ++letter) {
-					size_type pos = word.find(*letter);
-					while (pos != std::string::npos) {
-						auto word_start = letter - pos;
-						auto row_prev = word_start - 1;
-						auto row_next = word_start + word.size();
-						if ((row_prev < board_row.begin() || *row_prev == empty) && (row_next >= board_row.end() || *row_next == empty)) {
-							int score = evaluate_play(computer_available_letter_counts_, word_start, board_row.end(), word);
-							if (score > best_play.score) {
-								best_play = play{0, pos, word, score};
-							}
-						}
-						pos = word.find(*letter, pos + 1);
+				while (person_play.row == 0) {
+					out << "Invalid location format. Input the row integer, column letter (lowercase), and direction letter (either 'a' for 'across' or 'd' for 'down') without any separating characters. For example: 11gd" << std::endl;
+					person_location = get_input(person_location_prompt);
+					play person_play = parse_location(person_location);
+				}
+				person_play.word = person_word;
+				evaluate_play(person_available_letter_counts_, person_play);
+				if (person_play.score <= 0) {
+					out << "That word cannot be played there. Try again." << std::endl;
+					person_word = clean_word(get_input(person_word_prompt));
+					person_location = get_input(person_location_prompt);
+				}
+			}
+			person_score_ += person_play.score;
+
+			play computer_play = best_overall();
+			if (computer_play.score < 1) {
+				return false;
+			}
+			apply_play_to_board(computer_play);
+			computer_score_ += computer_play.score;
+			if (is_board_cramped()) {
+				return false;
+			}
+			return true;
+		}
+
+		bool is_board_cramped() {
+			size_type empty_count = 0;
+			for (const std::vector<char>& row : board_) {
+				for (const char& ch : row) {
+					if (ch == empty) {
+						++empty_count;
 					}
 				}
 			}
-			return best_play;
-		}
-
-		bool turn(std::string person_word, std::string person_location) {
-			return false;
-		}
-
-		bool board_cramped() {
-			return true;
+			return empty_count > board_dimension_ * board_dimension_;
 		}
 
 		std::array<int, letter_space_size> letter_scores() const noexcept {
@@ -109,39 +134,136 @@ namespace scrabble_sleuth {
 		int person_score_;
 		int computer_score_;
 
-		int evaluate_play(std::array<unsigned int, letter_space_size + 1>& available_letter_counts, std::vector<char>::const_iterator row_begin, std::vector<char>::const_iterator row_end, const std::string& word) {
-			int score = 0;
-			bool letters_used = false;
-			auto row_it = row_begin;
-			for (auto word_it = word.begin(); word_it < word.end(); ++word_it, ++row_it) {
-				size_type letter_as_index = letter_to_index(*word_it);
-				if (row_it < row_end) {
-					if (*row_it == *word_it) {
-						score += letter_scores_.at(letter_as_index);
-					}
-					else if (*row_it == empty && available_letter_counts.at(letter_as_index) > 0) {
-						letters_used = true;
-						--available_letter_counts.at(letter_as_index);
-						score += letter_scores_.at(letter_as_index);
-					}
-					else {
-						return -1;
-					}
+		play parse_location(std::string location) {
+			std::smatch location_match{};
+			play p{0, 0, true, "to be determined", -1};
+			if (regex_match(location, location_match, valid_location)) {
+				try {
+					p.row = std::stoi(location_match[1].str());
 				}
-				else {
-					return -1;
+				catch (...) {
+					return p;
+				}
+				p.col = letter_to_index(location_match[2].str().front());
+				p.across = location_match[3].str().front() == 'a';
+			}
+			return p;
+		}
+
+		play best_overall() {
+			play best_overall{0, 0, true, "to be determined", -1};
+			play curr_best;
+			for (size_type row_i = 0; row_i < board_dimension_; ++row_i) {
+				curr_best = best_in_row(row_i, true);
+				if (curr_best.score > best_overall.score) {
+					best_overall = curr_best;
 				}
 			}
-			if (letters_used) {
-				return score;
+			for (size_type col_i = 0; col_i < board_dimension_; ++col_i) {
+				curr_best = best_in_row(col_i, false);
+				if (curr_best.score > best_overall.score) {
+					best_overall = curr_best;
+				}
+			}
+			return best_overall;
+		}
+
+		play best_in_row(size_type row_index, bool is_row = true) {
+			std::vector<char> board_row;
+			if (is_row) {
+				board_row = board_.at(row_index);
 			}
 			else {
-				return -1;
+				board_row.reserve(board_dimension_);
+				for (size_type row_i = 0; row_i < board_dimension_; ++row_i) {
+					board_row.push_back(board_.at(row_i).at(row_index));
+				}
 			}
+			std::set<char> row_letters{};
+			for (const char& ch : board_row) {
+				if (ch != empty) {
+					row_letters.insert(ch);
+				}
+			}
+			play best_play{0, 0, true, "to be determined", -1};
+
+			for (const std::string& word : valid_words_) {
+				if (word.size() > board_dimension_) {
+					continue;
+				}
+				for (const char& letter : row_letters) {
+					size_type pos = word.find(letter);
+					while (pos != std::string::npos) {
+						auto word_start = board_row.begin() + pos;
+						auto word_end = word_start + word.size();
+						if ((word_start == board_row.begin() || *word_start - 1 == empty) && (word_end == board_row.end() || *word_end == empty)) {
+							play curr_play{};
+							if (is_row) {
+								curr_play = play{row_index, pos, is_row, word, -1};
+							}
+							else {
+								curr_play = play{pos, row_index, is_row, word, -1};
+							}
+							evaluate_play(computer_available_letter_counts_, curr_play);
+							if (curr_play.score > best_play.score) {
+								best_play = curr_play;
+							}
+						}
+						pos = word.find(letter, pos + 1);
+					}
+				}
+			}
+			return best_play;
+		}
+
+		void apply_play_to_board(play p) {
+			if (p.across) {
+				for (size_type pos = 0; pos < p.word.size(); ++pos) {
+					board_.at(p.row).at(p.col + pos) = p.word.at(pos);
+				}
+			}
+			// p.down
+			else {
+				for (size_type pos = 0; pos < p.word.size(); ++pos) {
+					board_.at(p.row + pos).at(p.col) = p.word.at(pos);
+				}
+			}
+		}
+
+		int evaluate_play(std::array<unsigned int, letter_space_size + 1>& available_letter_counts, play p) {
+			p.score = 0;
+			bool letters_used = false;
+			for (const char& ch : p.word) {
+				size_type letter_as_index = letter_to_index(ch);
+				try {
+					if (board_.at(p.row).at(p.col) == ch) {
+						p.score += letter_scores_.at(letter_as_index);
+					}
+					else if (board_.at(p.row).at(p.col) == empty && available_letter_counts.at(letter_as_index) > 0) {
+						letters_used = true;
+						--available_letter_counts.at(letter_as_index);
+						p.score += letter_scores_.at(letter_as_index);
+					}
+					else {
+						p.score = -1;
+						break;
+					}
+				}
+				catch (std::out_of_range) {
+					p.score = -1;
+					break;
+				}
+			}
+			if (!letters_used) {
+				p.score = -1;
+			}
+			return p.score;
 		}
 
 		static size_type letter_to_index(char le);
 	};
+
+	const std::regex ScrabbleGame::valid_location{"(\\d{1,2})([a-z])([ad])"};
 
 	size_type ScrabbleGame::letter_to_index(char le) {
 		if (le == '*') {
@@ -151,21 +273,14 @@ namespace scrabble_sleuth {
 	}
 
 	std::string ScrabbleGame::clean_word(std::string word) {
-		for (auto it = word.begin(); it < word.end(); ++it) {
-			*it = std::tolower(static_cast<unsigned char>(*it));
+		for (char& ch : word) {
+			ch = std::tolower(static_cast<unsigned char>(ch));
 			// letter_to_index assumes the char is lowercase ASCII, so underflow may occur here
-			if (ScrabbleGame::letter_to_index(*it) >= ScrabbleGame::letter_space_size) {
+			if (ScrabbleGame::letter_to_index(ch) >= ScrabbleGame::letter_space_size) {
 				return {""};
 			}
 		}
 		return word;
-	}
-
-	std::string get_input(std::string prompt, std::istream& in = std::cin, std::ostream& out = std::cout) {
-		out << prompt << " " << std::flush;
-		std::string input;
-		in >> input;
-		return input;
 	}
 
 	int play_scrabble(std::string letter_scores_path, std::string valid_words_path, size_type board_dimension, std::istream& in = std::cin, std::ostream& out = std::cout) {
@@ -191,18 +306,10 @@ namespace scrabble_sleuth {
 		ScrabbleGame game{letter_scores, valid_words, board_dimension};
 		std::string person_word;
 		std::string person_location;
-		std::smatch person_location_match;
 		do {
-			person_word = get_input("What word do you want to play?");
-			person_location = get_input("Where do you want to play the word?");
-			std::regex valid_location{"(\\d{1,2})[a-z][ad]"};
-			bool person_location_valid = regex_match(person_location, person_location_match, valid_location);
-			while (!person_location_valid) {
-				out << "Invalid location format. Input the row integer, column letter (lowercase), and direction letter (either 'a' for 'across' or 'd' for 'down') without any separating characters. For example: 11gd" << std::endl;
-				person_location = get_input("Where do you want to play the word?");
-				person_location_valid = regex_match(person_location, person_location_match, valid_location);
-			}
-		} while (game.turn(person_word, person_location));
+			person_word = ScrabbleGame::clean_word(get_input(person_word_prompt));
+			person_location = get_input(person_location_prompt);
+		} while (game.turn(person_word, person_location, out));
 
 		if (game.person_score() > game.computer_score()) {
 			out << "Congradulations, you beat Scrabble Sleuth." << std::endl;
